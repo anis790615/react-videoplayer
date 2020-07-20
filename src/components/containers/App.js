51b463a1372796672052a6a2a886d3cdc22069d6
Anis Alkomem <anis.alkumaim@gmail.com>
2020-07-20 15:02:34 +0200

    Changed basename

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const App = () => (
-  <BrowserRouter basename="/react_videoplayer/">
+  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Player} />
       <Route exact path="/:activeVideo" component={Player} />
